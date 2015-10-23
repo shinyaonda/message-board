48526596d9ff6dbf4967b891afe437e0e00c8a41
shinyaonda <shinya.o.2010@gmail.com>
2015-10-23 01:08:47 +0000

    add_edit_second

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
@@ -6,11 +7,6 @@ class MessagesController < ApplicationController
   end
 
   ##ここから追記
-  def create
-    @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice:'メッセージを保存しました'
- end
  
  def create
    @message = Message.new(message_params)
@@ -24,9 +20,27 @@ class MessagesController < ApplicationController
     end
   end  
  
+def update
+  if @message.update(message_params)
+    #保存に成功した場合はトップページへリダイレクト
+    redirect_to root_path , notice: 'メッセージを編集しました'
+  else
+    #保存に失敗した場合は編集画面へ戻す
+    render 'edit'
+  end
+end  
+
  private
+ 
+ def set_message
+   @message = Message.find(params[:id])
+ end
+ end
+
  def message_params
    params.require(:message).permit(:name, :body)
   end
   ## ここまで
+
+def edit
 end
